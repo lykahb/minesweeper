4bb3e8e91509408c9295fd20575b1efec60a119e
Boris Lykah <lykahb@gmail.com>
2015-08-14 22:40:06 -0400

    Handle creation of new game

--- minesweeper/views.py
+++ minesweeper/views.py
@@ -1,36 +1,39 @@
-from pyramid.response import Response
+import random
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
     Game,
-    PlayerAction
-)
+    PlayerAction,
+    CellState)
+
+
+# Creates a board with given dimensions that has no mines on the start position
+def init_game_state(width, height, start_x, start_y):
+    mine_probability = 0.3
+    board = [[0 for _ in range(width)] for _ in range(height)]
+    for x in range(width):
+        for y in range(height):
+            if x == start_x and y == start_y:
+                board[x][y] = CellState.empty.value
+            else:
+                board[x][y] = CellState.mine.value if random.random() < mine_probability else CellState.empty.value
+    return board
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = 'one'  # DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'minesweeper'}
+def home(request):
+    return {}
 
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_minesweeper_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+@view_config(route_name='new_game', renderer='json')
+def new_game(request):
+    width = int(request.POST['width'])
+    height = int(request.POST['height'])
+    x = int(request.POST['x'])
+    y = int(request.POST['y'])
+    game = Game(board_state=init_game_state(width, height, x, y))
+    DBSession.add(game)
+    DBSession.flush()
+    return {'game_id': game.id}
